fd24c26a62ccdf2005f56b204ec9f062a615936f
luisEfe80 <luismendez80@hotmail.com>
2021-11-11 19:42:56 -0500

    adicion de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,7 +6,15 @@
       .titulo-principal__numero
         span 1
       h1 Conceptos de logística y mercancías
-
+    .row.mb-5
+      .col-lg-7
+        .cajon-b.color-primario.p-3.mb-4
+          p.mb-0  A lo largo de años, la funcionalidad logística fue considerada como una actividad rutinaria, meramente operativa y elemental para hacer llegar los productos a partir de los centros de producción a los de uso o consumo. A partir de esta visión, la funcionalidad logística en la organización era contemplada solo como un centro generador de costes sin capacidad de diferenciación (Ballou, 2004). 
+        p.mb-3  En el campo académico, su análisis se realizaba a partir del punto de vista industrial o técnica, en busca de modelos que permitieran optimizar espacios, planear distribuciones y minimizar costes. La globalización de la economía y la consiguiente apertura de nuevos mercados distanciados geográficamente fueron cambiando la percepción de la funcionalidad logística hacia novedosas magnitudes, en las que la funcionalidad logística permitía ciertas ventajas competitivas fundamentadas en la reducción de los tiempos de entrega o en la mejora de costes.
+        p.mb-3  Sin embargo, a pesar de estas ventajas, la funcionalidad logística seguía siendo una actividad menor en la administración empresarial. En la década de 1960 la administración logística, siguiendo los postulados de marketing, consigue una orientación hacia el comprador que se materializa en dar un servicio logístico ajustado a las necesidades y requisitos de este, y que posibilite su satisfacción. A partir de allí, la funcionalidad logística comienza a ser tratada como una actividad de carácter estratégico capaz de crear ventajas competitivas que diferencian a la organización de la competencia. A continuación, se relacionan aspectos importantes sobre el proceso de logística.
+      .col-lg-5
+        figure
+          img(src='@/assets/curso/t1-i1.jpg' alt='')
 
     
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,15 +1,27 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.bg-foot
     .titulo-principal.color-acento-contenido(data-aos='flip-up')
       .titulo-principal__numero
         span 2
       h1 Ficha técnica de mercancías
 
+    .row.mb-5
+      .col-lg-7.slideInLeft
+        .cajon-b.color-primario.p-3.mb-4
+          p.mb-0  La ficha técnica es un documento en forma de sumario que contiene la descripción de las características técnicas de un objeto, material, producto o bien de manera detallada. Los contenidos varían dependiendo del producto, servicio o entidad descrita, pero en general contiene datos como el nombre, características físicas, el modo de uso o elaboración, propiedades distintivas, métodos de ensayo y especificaciones técnicas.
+        p.mb-3  La preparación de la ficha técnica posibilita adecuar los productos mediante la estandarización de los productos, de esta forma se describe la calidad requerida por el comprador, además posibilita a este conocer la información técnica del producto o los productos que está adquiriendo y, así mismo, ofrecer cumplimiento a las exigencias que la ley exige en la normatividad presente; lo anterior posibilita evitar inconvenientes económicos y facilitar la verificación de calidad de producto.
+      .col-lg-5.slideInRight
+        figure
+          img(src='@/assets/curso/t2-i1.jpg' alt='')
 
-    
-
+    .row.mb-5.justify-content-center
+      .col-lg-8
+        .cajon.color-secundario.p-3
+          .row
+            .col-lg-6   
+            .col-lg-6  
 
 
 
